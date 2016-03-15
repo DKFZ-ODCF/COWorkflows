ea1e581e131eabd6eabc45d0af9314b43a26274a
Philip R. Kensche <kensche@dkfz-heidelberg.de>
2016-03-15 18:46:19 +0100

    Something to start: Extractor to get information from different sources (file names, tables, DB, etc.).

---
+++ src/de/dkfz/b080/co/common/TableBasedInputFileInformationExtractor.groovy
@@ -0,0 +1,26 @@
+package de.dkfz.b080.co.common
+
+@groovy.transform.CompileStatic
+class TableBasedInputFileInformationExtractor {
+
+    String getSampleName() {
+
+    }
+
+    String getLibraryId () {
+
+    }
+
+    String getPlatformId () {
+
+    }
+
+    String getSampleType () {
+
+    }
+
+    String getPairingType () {
+
+    }
+
+}

a3a8acb16598016fa305c9891b81c38b5071e783
Philip R. Kensche <p.kensche@dkfz-heidelberg.de>
2017-08-15 10:28:30 +0200

    Changes for Roddy 2.4.

--- src/de/dkfz/b080/co/files/LaneFile.java
+++ src/de/dkfz/b080/co/files/LaneFile.java
@@ -1,16 +1,15 @@
 package de.dkfz.b080.co.files;
 
-import de.dkfz.roddy.Roddy;
 import de.dkfz.roddy.config.Configuration;
-import de.dkfz.roddy.config.ConfigurationValue;
 import de.dkfz.roddy.core.ExecutionContext;
 import de.dkfz.roddy.execution.io.ExecutionResult;
 import de.dkfz.roddy.execution.io.ExecutionService;
 import de.dkfz.roddy.execution.io.fs.FileSystemAccessProvider;
 import de.dkfz.roddy.execution.jobs.Job;
 import de.dkfz.roddy.execution.jobs.JobResult;
-import de.dkfz.roddy.execution.jobs.cluster.pbs.ChangedProcessDependencyProcessingCommand;
-import de.dkfz.roddy.knowledge.files.*;
+import de.dkfz.roddy.knowledge.files.BaseFile;
+import de.dkfz.roddy.knowledge.files.FileGroup;
+import de.dkfz.roddy.knowledge.files.ITestdataSource;
 import de.dkfz.roddy.knowledge.methods.GenericMethod;
 import de.dkfz.roddy.tools.LoggerWrapper;
 
@@ -20,7 +19,6 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  * @author michael
